8b693ab393d92278ae14d001a3a007762e50a4ac
Phil Hartley <philh725@gmail.com>
2018-10-25 01:41:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,8 +22,10 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
-  def input_to_index(input)
-    
+  def input_to_index(user_input)
+    user_input = user_input.to_i
+    user_input -= 1
+  end
   
   
 end
